ad86052d640f6f39b8b47426afb06d0a731c9eff
Thomas van de Meer <t.vandemeer@gmail.com>
2024-02-16 14:09:17 +0100

    Commit na video 11

--- AspNet8/Interfaces/IStockRepository.cs
+++ AspNet8/Interfaces/IStockRepository.cs
@@ -1,9 +1,18 @@
-﻿using AspNet8.Models;
+﻿using AspNet8.DTOs.Stock;
+using AspNet8.Models;
 
 namespace AspNet8.Interfaces
 {
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
+
+        Task<Stock?> GetByIdAsync(int id);
+
+        Task<Stock> CreateAsync(Stock stockModel);
+
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+
+        Task<Stock?> DeleteAsync(int id);
     }
 }
