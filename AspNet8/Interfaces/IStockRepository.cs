acd21fef47fc26d093d576f1b50852b15bcaa7d2
Thomas van de Meer <t.vandemeer@gmail.com>
2024-02-16 10:58:25 +0100

    Commit na video over repositories

---
+++ AspNet8/Interfaces/IStockRepository.cs
@@ -0,0 +1,9 @@
+﻿using AspNet8.Models;
+
+namespace AspNet8.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllAsync();
+    }
+}
---
+++ AspNet8/Repository/StockRepository.cs
@@ -0,0 +1,22 @@
+﻿using AspNet8.Data;
+using AspNet8.Interfaces;
+using AspNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNet8.Repository
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Stock>> GetAllAsync()
+        {
+            return _context.Stocks.ToListAsync();
+        }
+    }
+}
